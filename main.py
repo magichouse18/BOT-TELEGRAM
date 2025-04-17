dea7ee3b8c725c098765e5aba5f4f11632aa8556
magichouse18 <conti9708@gmail.com>
2025-04-17 14:47:52 +0200

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler, Updater
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
-TOKEN = "inserisci_il_tuo_token_qui"
+TOKEN = "7829595925:AAFg1zi_RWGPbIEXRTazPuITJso7m36rhpQ"
 
-# Costanti per i tasti
-SHOP, WEED, HASH, EDIBILI, REGOLAMENTO, PAGAMENTI, CONTATTI = range(7)
-GREENPOISON, CALIUSA = range(7, 9)
+# --- Costanti per i tasti ---
+SHOP, PAGAMENTI, CONTATTI = range(3)
+WEED, HASH, EDIBILI, REGOLAMENTO = range(4, 8)
+CALI_USA, GREEN_POISON = range(8, 10)
+FILTRATI, FROZEN = range(10, 12)
+VAPE_PEN = 12
 
 def start(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [InlineKeyboardButton("🛍️ Shop 🛍️", callback_data=str(SHOP))],
         [InlineKeyboardButton("💰 Pagamenti 💰", callback_data=str(PAGAMENTI))],
-        [InlineKeyboardButton("👤 Contattami 👤", callback_data=str(CONTATTI))],
+        [InlineKeyboardButton("👤 Contattami 👤", callback_data=str(CONTATTI))]
     ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
-        "Benvenuti nel nostro Bot Ufficiale. Il Bot viene costantemente aggiornato con i prodotti disponibili. Per qualsiasi informazione o ordini contattare: @theitalianfactory_official 📲",
-        reply_markup=reply_markup,
+        "Benvenuti nel nostro Bot Ufficiale.\n"
+        "Il Bot viene costantemente aggiornato con i prodotti disponibili. "
+        "Per qualsiasi informazione o ordini contattare: @theitalianfactory_official 📲",
+        reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
 def button(update: Update, context: CallbackContext) -> None:
@@ -26,53 +30,110 @@ def button(update: Update, context: CallbackContext) -> None:
 
     if data == SHOP:
         keyboard = [
-            [InlineKeyboardButton("🍁 WEED 🍁", callback_data=str(WEED))],
-            [InlineKeyboardButton("🍫 HASH 🍫", callback_data=str(HASH))],
-            [InlineKeyboardButton("🍬 EDIBILI 🍬", callback_data=str(EDIBILI))],
-            [InlineKeyboardButton("📖 REGOLAMENTO 📖", callback_data=str(REGOLAMENTO))],
-            [InlineKeyboardButton("⬅️ Indietro", callback_data="/start")],
+            [InlineKeyboardButton("🍁 WEED 🍁", callback_data=str(WEED)),
+             InlineKeyboardButton("🍫 HASH 🍫", callback_data=str(HASH))],
+            [InlineKeyboardButton("🍭 EDIBILI 🍭", callback_data=str(EDIBILI)),
+             InlineKeyboardButton("📖 REGOLAMENTO 📖", callback_data=str(REGOLAMENTO))],
+            [InlineKeyboardButton("⬅️ Indietro", callback_data="start")]
         ]
-        query.edit_message_text("Seleziona il servizio per ricevere maggiori informazioni.", reply_markup=InlineKeyboardMarkup(keyboard))
+        query.edit_message_text("Seleziona il servizio per ricevere maggiori informazioni.",
+                                reply_markup=InlineKeyboardMarkup(keyboard))
 
     elif data == WEED:
         keyboard = [
-            [InlineKeyboardButton("🇺🇸 CALI USA 🇺🇸", callback_data=str(CALIUSA))],
-            [InlineKeyboardButton("🌴 GREEN POISON 🌴", callback_data=str(GREENPOISON))],
-            [InlineKeyboardButton("⬅️ Indietro", callback_data=str(SHOP))],
+            [InlineKeyboardButton("🇺🇸 CALI USA 🇺🇸", callback_data=str(CALI_USA))],
+            [InlineKeyboardButton("🌴 GREEN POISON 🌴", callback_data=str(GREEN_POISON))],
+            [InlineKeyboardButton("⬅️ Indietro", callback_data=str(SHOP))]
         ]
-        query.edit_message_text("La nostra selezione delle migliori erbe disponibili al momento:", reply_markup=InlineKeyboardMarkup(keyboard))
+        query.edit_message_text("La nostra selezione delle migliori erbe disponibili al momento:",
+                                reply_markup=InlineKeyboardMarkup(keyboard))
 
-    elif data == GREENPOISON:
-        query.message.delete()
+    elif data == GREEN_POISON:
         context.bot.send_photo(
             chat_id=query.message.chat_id,
-            photo=open("green_poison.jpg", "rb"),
+            photo=open("green_poison.jpeg", "rb"),
             caption=(
-                """🌴 GREEN POISON (✅)
-🏡 INDOOR
-
-💰PREZZO:
-10g👉90€
-15g👉125€
-20g👉160€
-30g👉225€
-50g👉375€
-100g👉675€
-200g👉1300€
-500g👉3125€
-
-ℹ️Coltivata in serra, cime belle piene. NO SEMI, cime compatte sia tozze sia lunghe"""
+                "🌴 GREEN POISON (✅)\n🏡 INDOOR\n\n"
+                "💰PREZZO:\n"
+                "10g👉90€\n15g👉125€\n20g👉160€\n30g👉225€\n"
+                "50g👉375€\n100g👉675€\n200g👉1300€\n500g👉3125€\n\n"
+                "ℹ️ Coltivata in serra, cime belle piene. NO SEMI, cime compatte sia tozze sia lunghe."
             ),
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("⬅️ Indietro", callback_data=str(WEED))]]
-            ),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data=str(WEED))]])
         )
 
-def main():
+    elif data == CALI_USA:
+        query.edit_message_text("🧪 Sezione in aggiornamento!",
+                                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data=str(WEED))]]))
+
+    elif data == HASH:
+        keyboard = [
+            [InlineKeyboardButton("🍫 FILTRATI 🍫", callback_data=str(FILTRATI))],
+            [InlineKeyboardButton("❄️ FROZEN ❄️", callback_data=str(FROZEN))],
+            [InlineKeyboardButton("⬅️ Indietro", callback_data=str(SHOP))]
+        ]
+        query.edit_message_text("La nostra selezione di hash per tutte le tipologie di smokers:",
+                                reply_markup=InlineKeyboardMarkup(keyboard))
+
+    elif data == FILTRATI:
+        context.bot.send_photo(
+            chat_id=query.message.chat_id,
+            photo=open("dubai_chocolate.jpeg", "rb"),
+            caption=(
+                "🌏🍫DUBAI CHOCOLATE 73u (✅)\n-FILTRATO 73u\n\n"
+                "💰PREZZO:\n10g 👉 95€\n15g 👉 125€\n20g 👉 165€\n30g 👉 230€\n"
+                "50g 👉 370€\n100g 👉 700€\n\n"
+                "ℹ️Profilo Aromatico/Fruttato con note tropicale di agrumi"
+            ),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data=str(HASH))]])
+        )
+
+    elif data == FROZEN:
+        query.edit_message_text("❄️ Sezione FROZEN al momento non disponibile ❄️",
+                                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data=str(HASH))]]))
+
+    elif data == EDIBILI:
+        keyboard = [
+            [InlineKeyboardButton("VAPE PEN", callback_data=str(VAPE_PEN))],
+            [InlineKeyboardButton("⬅️ Indietro", callback_data=str(SHOP))]
+        ]
+        query.edit_message_text("Sezione edibili disponibile:",
+                                reply_markup=InlineKeyboardMarkup(keyboard))
+
+    elif data == REGOLAMENTO:
+        query.edit_message_text(
+            "Leggere attentamente il regolamento sul servizio che desiderate. "
+            "Prima di qualsiasi ordine vi verrà richiesta la Verifica.",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data=str(SHOP))]])
+        )
+
+    elif data == PAGAMENTI:
+        query.edit_message_text("💳 Metodo di pagamento disponibile: solo criptovalute.",
+                                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data="start")]]))
+
+    elif data == CONTATTI:
+        query.edit_message_text("Contattaci su Telegram: @theitalianfactory_official",
+                                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data="start")]]))
+
+    elif data == VAPE_PEN:
+        query.edit_message_text(
+            "JEETER JUICE 1G THC PODS 🧪\n"
+            "~ Slurricane\n~ Chemdog\n~ Banana Mango\n~ Lemon Drop\n\n"
+            "1pz → 60€\n2pz → 100€\n5pz → 225€\n10pz → 400€\n50pz → 1750€\n100pz → 2700€\n\n"
+            "Vere penne da Cali con QR e marchio originale. No fake homemade.",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Indietro", callback_data=str(EDIBILI))]])
+        )
+
+    elif query.data == "start":
+        start(update, context)
+
+def main() -> None:
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
+
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CallbackQueryHandler(button))
+
     updater.start_polling()
     updater.idle()
 
